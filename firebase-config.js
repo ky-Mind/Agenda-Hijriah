f82c07d0680beacfd8df05ed2db8b5d694cc2394
Hilmy <hilmykia@gmail.com>
2026-08-15 18:28:16 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -8,6 +8,6 @@ globalThis.AIH_FIREBASE_CONFIG = {
   storageBucket: "kisah-2cc70.firebasestorage.app",
   messagingSenderId: "417572719269",
   appId: "1:417572719269:web:ae6d40418e51b167441b4c",
-  measurementId: "G-4NBFJ03VW4",
+  measurementId: "G-YLCLS6WJ03",
   vapidKey: "BC_HIDEQc2BH9NrYYZ7DIXcrD2tfo3juNKczdVO1kJv1sYA9eWDtXlQQxd89E-0ko_g0A-TesIyKg-NiMLzkaaI"
 };
